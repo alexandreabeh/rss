88b399abd995a34ef9ff85ee059c16c53a18910d
Mirco Macrelli <mmacrelli@gmail.com>
2013-09-28 11:18:00 +0200

    sposta la  factory fuori dai test

--- src/test/java/net/mircomacrelli/rss/RSSFactoryTest.java
+++ src/test/java/net/mircomacrelli/rss/RSSFactoryTest.java
@@ -1,5 +1,6 @@
 package net.mircomacrelli.rss;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -8,9 +9,15 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public final class RSSFactoryTest {
+    private static RSSFactory factory;
+
+    @BeforeClass
+    public static void setup() {
+        factory = RSSFactory.newFactory();
+    }
+
     @Test
     public void testParse() throws Exception {
-        final RSSFactory factory = RSSFactory.newFactory();
         try (InputStream in = RSS.class.getResourceAsStream("/rss-2.0-complete.xml")) {
             assertNotNull(factory.parse(in));
         }
@@ -18,7 +25,6 @@ public final class RSSFactoryTest {
 
     @Test
     public void parseIgnoreAllTheExtensionsInChannelAndItem() throws Exception {
-        final RSSFactory factory = RSSFactory.newFactory();
         try (InputStream in = RSS.class.getResourceAsStream("/rss-2.0-with-extensions.xml")) {
             assertNotNull(factory.parse(in));
         }
@@ -26,7 +32,6 @@ public final class RSSFactoryTest {
 
     @Test
     public void useAnEmptyStringIfTagHasNoText() throws Exception {
-        final RSSFactory factory = RSSFactory.newFactory();
         try (InputStream in = RSS.class.getResourceAsStream("/rss-2.0-tags-without-text.xml")) {
             assertTrue(factory.parse(in).getChannel().getDescription().isEmpty());
         }
