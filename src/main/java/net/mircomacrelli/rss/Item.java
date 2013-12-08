d8619cf340d2021a352f3db2521d6cebdcc81e27
Mirco Macrelli <mmacrelli@gmail.com>
2013-12-08 10:06:02 +0100

    rimuove import inutile

--- src/main/java/net/mircomacrelli/rss/Item.java
+++ src/main/java/net/mircomacrelli/rss/Item.java
@@ -12,7 +12,6 @@ import javax.xml.stream.events.StartElement;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
